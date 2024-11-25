9836a4b26cd0a11167075b5ea58776022556c187
m-yoshii-git <tn21.yoshii421@gmail.com>
2024-11-25 13:19:32 +0900

    string boot_easylogin

---
+++ src/test/java/jp/co/internous/easylogin2/Easylogin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.internous.easylogin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Easylogin2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
